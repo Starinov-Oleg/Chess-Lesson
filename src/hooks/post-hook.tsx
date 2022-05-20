4f414a02fa37cf682a127ddd551727e1f587902a
StarinovOleg <hromoi@inbox.ru>
2022-05-20 16:40:37 +0700

    add initialdata to index and fix post customise hook

--- src/hooks/post-hook.tsx
+++ src/hooks/post-hook.tsx
@@ -1,19 +1,8 @@
-import { useEffect } from 'react'
 import { useQuery } from 'react-query'
 import { PostService } from '../api/post-service'
-import { useLocation } from 'react-router-dom'
 
 const usePost = (id: any) => {
-  const location = useLocation()
-
-  const { refetch, data } = useQuery('articles', () => PostService.getPostId(id))
-
-  useEffect(() => {
-    if (location) {
-      refetch()
-    }
-  }, [location, refetch])
-
+  const { data } = useQuery(['articles', id], () => PostService.getPostId(id))
   return data
 }
 
