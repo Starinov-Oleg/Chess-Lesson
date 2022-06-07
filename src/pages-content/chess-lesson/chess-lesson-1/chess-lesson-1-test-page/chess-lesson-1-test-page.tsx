644f5e4e7b371df7be6b872af28082b5c0dee79c
StarinovOleg <hromoi@inbox.ru>
2022-06-07 10:38:15 +0700

    UI test

--- src/pages-content/chess-lesson/chess-lesson-1/chess-lesson-1-test-page/chess-lesson-1-test-page.tsx
+++ src/pages-content/chess-lesson/chess-lesson-1/chess-lesson-1-test-page/chess-lesson-1-test-page.tsx
@@ -3,11 +3,21 @@ import { Col, Row, Container } from 'react-bootstrap'
 import CardTest from '../../../../ui-library/card-test/card-test'
 import CardTestResult from '../../../../ui-library/card-test/card-test-result'
 function ChessLessonTestPage() {
+  const lists = [
+    { id: 0, list_item_label: 'hi' },
+    { id: 1, list_item_label: 'hi' },
+  ]
   return (
     <Container>
       <Row className='row'>
         <Col lg={12} xs={12}>
-          <CardTest title='Test move piece' description='here text about test' />
+          <CardTest
+            title='Test move piece'
+            description='here text about test'
+            question='First question?'
+            chose_test='checkbox'
+            lists={lists}
+          />
           <CardTestResult />
           <Button message='Back on Lesson page>>' href='/chesslesson' />
         </Col>
--- src/ui-library/card-test/card-test.tsx
+++ src/ui-library/card-test/card-test.tsx
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import H3 from '../h3/h3'
+import Label from '../../ui-library/label/label'
 interface CardTestProps {
   title: string
   description: string
+  question: string
+  chose_test: string
+  lists: Array<any>
 }
 
 const StyledCardTest = styled.div``
-function CardTest({ title, description }: CardTestProps) {
+const StyledQuestion = styled.div``
+const StyledCheckbox = styled.div`
+  label {
+    padding-right: 1rem;
+    padding-left: 1rem;
+  }
+`
+const StyledRadio = styled.div`
+  label {
+    padding-right: 1rem;
+    padding-left: 1rem;
+  }
+`
+const StyledTestItem = styled.div``
+
+function CardTest({ title, description, question, chose_test, lists }: CardTestProps) {
+  const listItems_checkbox = lists.map(list => (
+    <Fragment key={list.id}>
+      <input type='checkbox' id={list.list_item_label} name={list.list_item_label} value={list.list_item_label} />
+      <Label message={list.list_item_label} htmlFor={list.list_item_label} />
+    </Fragment>
+  ))
+  const listItems_radio = lists.map(list => (
+    <Fragment key={list.id}>
+      <input type='radio' id={list.list_item_label} name={list.list_item_label} value={list.list_item_label} />
+      <Label message={list.list_item_label} htmlFor={list.list_item_label} />
+    </Fragment>
+  ))
   return (
     <StyledCardTest className={`cardtest`}>
       <H3 message={title} />
       <p>{description}</p>
+      <StyledTestItem>
+        <StyledQuestion>{question}</StyledQuestion>
+        {chose_test === 'checkbox' ? (
+          <StyledCheckbox>
+            <form>{listItems_checkbox}</form>
+          </StyledCheckbox>
+        ) : (
+          <StyledRadio>
+            <form>{listItems_radio}</form>
+          </StyledRadio>
+        )}
+      </StyledTestItem>
     </StyledCardTest>
   )
 }
