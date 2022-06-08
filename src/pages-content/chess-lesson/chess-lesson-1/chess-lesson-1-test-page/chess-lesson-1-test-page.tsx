678ddca969da307c9886e69103253ba40187791b
StarinovOleg <hromoi@inbox.ru>
2022-06-08 09:19:40 +0700

    add listener for check

--- src/pages-content/chess-lesson/chess-lesson-1/chess-lesson-1-test-page/chess-lesson-1-test-page.tsx
+++ src/pages-content/chess-lesson/chess-lesson-1/chess-lesson-1-test-page/chess-lesson-1-test-page.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { Col, Container,Row } from 'react-bootstrap'
+import { ChangeEvent, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 import ButtonClick from '../../../../ui-library/button-click/button'
 import Button from '../../../../ui-library/button-link/button-link'
@@ -7,16 +7,20 @@ import CardTest from '../../../../ui-library/card-test/card-test'
 import CardTestResult from '../../../../ui-library/card-test/card-test-result'
 
 const styled_button = { margin: '1rem', padding: '1rem 4rem' }
-function handleSubmit(setShowResults: any) {
+function handleSubmit(setShowResults: (active: boolean) => void) {
   setShowResults(true)
   console.log('Отправлена форма.')
+}
+function checkValue(e: ChangeEvent<HTMLInputElement>) {
+  const value = e.target.value
+  console.log('You selected' + value)
 }
 function ChessLessonTestPage() {
   const [showResults, setShowResults] = useState(false)
 
   const lists = [
-    { id: 0, list_item_label: 'test', list_item_img: '' },
-    { id: 1, list_item_label: 'test', list_item_img: '' },
+    { id: 0, list_item_label: 'test1', list_item_img: '' },
+    { id: 1, list_item_label: 'test2', list_item_img: '' },
   ]
 
   return (
@@ -31,6 +35,7 @@ function ChessLessonTestPage() {
                 question='First question?'
                 chose_test='checkbox'
                 lists={lists}
+                onChange={checkValue}
               />
               <ButtonClick
                 message='Check'
