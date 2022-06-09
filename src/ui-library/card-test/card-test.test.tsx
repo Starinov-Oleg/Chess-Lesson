4c889637e16b09b0b5e49b934f686c4fa50c452e
StarinovOleg <hromoi@inbox.ru>
2022-06-09 08:58:27 +0700

    fix missing parameters

--- src/ui-library/card-test/card-test.test.tsx
+++ src/ui-library/card-test/card-test.test.tsx
@@ -4,6 +4,6 @@ import CardTest from './card-test'
 
 describe('card for test page', () => {
   it('card-test  render', () => {
-    render(<CardTest title={''} description={''} question={''} chose_test={''} lists={[]} />)
+    render(<CardTest title={''} description={''} question={''} chose_test={''} lists={[]} onChange={undefined} />)
   })
 })
