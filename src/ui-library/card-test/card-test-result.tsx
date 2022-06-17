d96fa7e75fd9283c1a7449e7ac80abc96cb13c37
StarinovOleg <hromoi@inbox.ru>
2022-06-17 14:56:26 +0700

    style for lesson

--- src/ui-library/card-test/card-test-result.tsx
+++ src/ui-library/card-test/card-test-result.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 
+import { PaddingTop } from '../../common/styled-components/padding-margin.styled'
 import H3 from '../h3/h3'
 
 interface CardTestResultProps {
@@ -11,9 +12,11 @@ const StyledCardTest = styled.div``
 function CardTestResult({ message_result, count }: CardTestResultProps) {
   return (
     <StyledCardTest className={`cardtestresult`}>
-      <H3 message={message_result}>
-        <span>{count}</span>
-      </H3>
+      <PaddingTop>
+        <H3 message={message_result}>
+          <span>{count}</span>
+        </H3>
+      </PaddingTop>
     </StyledCardTest>
   )
 }
