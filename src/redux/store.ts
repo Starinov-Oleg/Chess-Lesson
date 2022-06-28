c333180bbb71f5079fa7016a7d11d595497469d2
StarinovOleg <hromoi@inbox.ru>
2022-06-28 15:20:44 +0700

    remove not using reducer and correct state

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import pieceReducer from './reducer/board-game-pieces-reducer'
-import boardGameReducer from './reducer/board-game-reducer'
 import contactReducer from './reducer/contact-reducer'
 import eventReducer from './reducer/event-reducer'
 import authReducer from './slicer/auth-slicer'
 import postReducer from './slicer/user-activity-slicer'
-//import authReducer from './reducer/auth.reducer'
 import itemReducer from './slicer/user-slicer'
 export default configureStore({
   reducer: {
     events: eventReducer,
     contacts: contactReducer,
-    board: boardGameReducer,
-    piece: pieceReducer,
     auth: authReducer,
     users: itemReducer,
     posts: postReducer,
